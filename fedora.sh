e8e450d696d3c161eb04cfa68f17af8ccaf39350
phoenix <136329@gmail.com>
2019-03-31 14:19:21 +0430

    add step 2

--- fedora.sh
+++ fedora.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #این برنامه برای کاربران تازه کار فدورا درست شده است و مرتب هم به روزرسانی خواهد شد
 clear
-extit_program="y"
-while [ "$extit_program" == "y" ];do
+exit_program="y"
+while [ "$exit_program" == "y" ];do
   echo -e "adade marhale mored nazare khodetan ra entekhab bekonid:\n1)tarrif user root\t2)beroz resani(update)"
   read step
   case "$step" in
     "1")
     echo "dar ayandeh ezafe mishavad";;
     "2")
-    echo "ramz hesab karbary khod ra vared bekonid"
+    notify-send -t 3000 "رمز حساب کاربری خود را وارد کنید"
     sudo dnf -y update
-    notify-send -t 5000 "be roz resani anjam shod";;
+    notify-send -t 5000 "به روز رسانی انجام شد";;
   esac
-  read -p "aya mikhahid edame dahid(y/n)" extit_program
+  read -p "aya mikhahid edame dahid(y/n)" exit_program
 done
