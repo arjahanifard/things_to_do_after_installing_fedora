e46d6e0de4587db1aab62d6c17eef4ec680f0c65
phoenix <136329@gmail.com>
2019-04-02 15:02:08 +0430

    add step 4 and optimize code

--- fedora.sh
+++ fedora.sh
@@ -10,7 +10,8 @@ notify_func (){
 }
 exit_program="y"
 while [ "$exit_program" == "y" ];do
-  echo -e "adade marhale mored nazare khodetan ra entekhab bekonid:\n1)tarrif user root\t2)beroz resani(update)\t3)afzodane makhazen rpmfusion(add rpmfusion)"
+  echo -e "adade marhale mored nazare khodetan ra entekhab bekonid:\n1)tarrif user root\t2)beroz resani(update)\t3)afzodane makhazen rpmfusion(add rpmfusion)\n
+  4)afzodane codec soti va tesfiri"
   read step
   case "$step" in
     "1")
@@ -25,7 +26,7 @@ while [ "$exit_program" == "y" ];do
       sudo dnf  -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
       notify_func;;
       "4")
-      notify-send -t 3000 "نصب کدکهای تصویری برای بخش انواع فایلهای صوتی و تصویری"
+      notify-send -t 3000 "نصب کدکهای تصویری برای پخش فایلهای صوتی و تصویری"
       notify-send -t 3000 "رمز حساب کاربری خود را وارد کنید"
       sudo dnf -y install gstreamer gstreamer-ffmpeg gstreamer-plugins-bad-free gstreamer-plugins-bad-nonfree gstreamer-plugins-base gstreamer-plugins-good gstreamer-plugins-ugly gstreamer-plugins-bad ffmpeg libdvdread libdvdnav audacious-plugins-freeworld-mp3 phonon-backend-gstreamer xine-lib-extras-freeworld libva-intel-driver libva-intel-hybrid-driver python3-tkinter vim-enhanced vim-X11 gstreamer-plugins-good-extras gstreamer-devel gstreamer-plugins-base-devel gstreamer1-libav gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly lsdvd lame faad2 ffmpeg-libs
       notify_func;;
